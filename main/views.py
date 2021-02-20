6594c3bc6937c1336e6cd66619850e0309f36f91
h4cktivist <popovvadim0605@gmail.com>
2021-02-20 16:45:15 +0500

    feat: post and comment models add

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post, Comment
+
+
+def index(request):
+    posts = Post.objects.all()
+
+    context = {'posts': posts}
+    return render(request, 'index.html', context)
