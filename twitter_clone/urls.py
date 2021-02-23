be75be5887c33d8e351bc244a35bcb386e47a04d
h4cktivist <popovvadim0605@gmail.com>
2021-02-23 14:08:51 +0500

    feat: profile and profile pic add

--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
 
 if settings.DEBUG == True:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
