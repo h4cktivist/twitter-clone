d47b58349b987373871f8fd64fa4f3008c3be37e
h4cktivist <popovvadim0605@gmail.com>
2021-04-27 14:13:14 +0500

    fix: user url fix

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('<str:u_name>/', views.otherProfile, name='otherprofile'),
+    path('profile/<str:u_name>/', views.otherProfile, name='otherprofile'),
     path('followers/', views.followers, name='followers'),
     path('follow_unfollow/', views.follow_unfollow, name='follow_unfollow'),
     path('register/', views.register, name='register'),
