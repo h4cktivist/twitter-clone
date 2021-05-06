2a2e9d497a17d4fc32c6f227bb6f168b6326cbb2
h4cktivist <popovvadim0605@gmail.com>
2021-05-06 11:44:04 +0500

    fix: followers deleted

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('profile/<str:u_name>/', views.otherProfile, name='otherprofile'),
-    path('followers/', views.followers, name='followers'),
     path('follow_unfollow/', views.follow_unfollow, name='follow_unfollow'),
     path('register/', views.register, name='register'),
     path('login/', views.logIn, name='login'),
